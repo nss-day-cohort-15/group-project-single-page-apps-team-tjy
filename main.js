a76a7e03a598257404dfe081fefbb9de208b8a66
wierje <wier.je@gmail.com>
2016-07-28 16:13:44 -0500

    more edits

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 var chatBox = document.getElementById('chatBox');
 var userInput = document.getElementById('userInput');
-
+var clearButton = document.getElementById('clearButton');
   userInput.addEventListener("keyup", checkKey);
   function checkKey(event){
     console.log(event)
     if(event.keyCode === 13){
+      console.log(clearButton)
       addMessage();
     }
   }
@@ -12,28 +13,8 @@ var userInput = document.getElementById('userInput');
 function addMessage () {
   var message = userInput.value;
   chatBox.innerHTML += message;
- }
-
-// var chatBox = document.getElementById('chatBox');
-// var userInput = document.getElementById('userInput');
-// var userInput = document.getElementById('clearButton');
-
-// // check key event
-// userInput.addEventListener("keyup", checkkey);
-// function checkkey(event) {
-//  // console.log(event);
-//  if (event.keyCode === 13) {
-//  addmessage();
-//  }
-// }
-
-// function addmessage() {
-//  var message = userInput.value;
-//  chatBox.innerHTML += message;
-// }
-//  // addline to remove userInput
-var chatBox = document.getElementById('chatBox');
-
+  checkChatBox();
+   }
 
 // remove messages
 clearButton.addEventListener("click", clearMessage);
@@ -41,9 +22,18 @@ clearButton.addEventListener("click", clearMessage);
 function clearMessage(event) {
   console.log('addMessage is running')
   chatBox.innerHTML = "";
+  checkChatBox();
 }
 
-
-
-
-
+// This will disable the first button with a class of "button-message"
+ function checkChatBox () {
+  console.log(chatBox.innerHTML)
+  if (chatBox.innerHTML ===""){
+    console.log()
+    clearButton.disabled = true;
+    console.log(clearButton);
+}else{
+  clearButton.disabled = false;
+}
+userInput.value = "";
+}
